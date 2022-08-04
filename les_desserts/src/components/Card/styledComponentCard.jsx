4b06d62d1126d2c7cffb2d343d897c9b89bfbfb8
benjamin31200 <benjamindeneux@hotmail.com>
2022-08-04 13:51:40 +0200

    cancel gitignore

---
+++ les_desserts/src/components/Card/styledComponentCard.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Card = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 330px;
+  border: groove;
+  background-image: url(https://img.freepik.com/vecteurs-libre/fond-abstrait-blanc_23-2148806276.jpg?t=st=1657015296~exp=1657015896~hmac=06acbdcf93c50155e15bad313a461525dc62094e25985ae65c30773bc17ff5ff&w=996);
+  background-position: center;
+  background-clip: content-box;
+  background-size: cover;
+  border: 2px solid black;
+  border-radius: 22px;
+  position: sticky;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  justify-content: space-evenly;
+  height: 100%;
+  width: auto;
+`;
